7212451d7620e7387ecf1b0e7b5f08b7bb982181
akinori-nakayama <akinori.nakayama@play.jp>
2023-07-11 01:53:15 +0900

    3. Graph の resolver を作成する

--- start/server/src/datasources/launch.js
+++ start/server/src/datasources/launch.js
@@ -1,25 +1,25 @@
-const { RESTDataSource } = require('apollo-datasource-rest');
+const { RESTDataSource } = require("apollo-datasource-rest");
 
 class LaunchAPI extends RESTDataSource {
   constructor() {
     super();
-    this.baseURL = 'https://api.spacexdata.com/v2/';
-    }
-  async getAllLaunches() {
-    const response = await this.get('launches');
-    return Array.isArray(response)
-    ? response.map(launch => this.launchReducer(launch))
-    : [];
+    this.baseURL = "https://api.spacexdata.com/v2/";
   }
+
+  async getAllLaunches() {
+    const response = await this.get("launches");
+    console.log("、あ、あ、あ、あ、あ、", Array.isArray(response));
+    return Array.isArray(response)
+      ? response.map((launch) => this.launchReducer(launch))
+      : [];
+  }
+
   launchReducer() {
     return {
       id: launch.flight_number || 0,
       cursor: `${launch.launch_data_unix}`,
       site: launch.launch_site && launch.launch_site.site_name,
       mission: {
-        name: launch.mission_name,
-        missionPatchSmall: launch.links.mission_patch_small,
-        missionPatchLarch: launch.links.mission_patch,
       },
       rocket: {
         id: launch.rocket.rocket_id,
@@ -28,16 +28,17 @@ class LaunchAPI extends RESTDataSource {
       },
     };
   }
-  getLaunchById({ launchId }) {
-    const response = await this.get('launches, { flight_number: launchId });
+
+  async getLaunchById({ launchId }) {
+    const response = await this.get("launches", { flight_number: launchId });
     return this.launchReducer(response[0]);
   }
+
   getLaunchesByIds({ launcheIds }) {
     return Promise.all(
-      launchIds.map(launchId => this.getLaunchById({ launchId }));
+      launchIds.map((launchId) => this.getLaunchById({ launchId }))
     );
   }
-
 }
 
 module.exports = LaunchAPI;
