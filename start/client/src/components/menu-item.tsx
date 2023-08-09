9848c1415eb95a58910fa55cf94d3f567db86317
akinori-nakayama <green.green.0223.green@gmail.com>
2023-08-10 01:39:38 +0900

    整形

--- start/client/src/components/menu-item.tsx
+++ start/client/src/components/menu-item.tsx
@@ -1,19 +1,19 @@
-import styled from '@emotion/styled';
-import { css } from '@emotion/react';
-import { Link } from 'react-router-dom';
-import { colors, unit } from '../styles';
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+import { Link } from "react-router-dom";
+import { colors, unit } from "../styles";
 
 export const menuItemClassName = css({
   flexGrow: 1,
   width: 0,
-  fontFamily: 'inherit',
+  fontFamily: "inherit",
   fontSize: 20,
-  color: 'inherit',
+  color: "inherit",
   letterSpacing: 1.5,
-  textTransform: 'uppercase',
-  textAlign: 'center',
+  textTransform: "uppercase",
+  textAlign: "center",
   svg: {
-    display: 'block',
+    display: "block",
     width: 60,
     margin: `0 auto ${unit}px`,
     fill: colors.secondary,
@@ -21,7 +21,7 @@ export const menuItemClassName = css({
 });
 
 const MenuItem = styled(Link)(menuItemClassName, {
-  textDecoration: 'none',
+  textDecoration: "none",
 });
 
 export default MenuItem;
